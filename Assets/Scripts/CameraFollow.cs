5b9cc1b8a1b4a90b1f7ae6b5dac6fc0b3b7acf36
Biligt <biligtgursed1027@gmail.com>
2022-05-31 16:00:09 +0800

    added camera follow script

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Transform target;
+    private Vector3 offset;
     void Start()
     {
-        
+        offset = transform.position-target.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 targetPosition = target.position+offset;
+        targetPosition.x=0;
+        transform.position=targetPosition;
     }
 }
