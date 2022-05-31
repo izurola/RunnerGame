918645eaa483dd0cbd05b6d48ec5310254b2c86d
Biligt <biligtgursed1027@gmail.com>
2022-05-31 18:18:33 +0800

    added generate ground

---
+++ Assets/Scripts/GeneratePlane.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePlane : MonoBehaviour
+{
+    public GameObject ground;
+    Vector3 pos = new Vector3(0,0,0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        int n = Random.Range(6,10);
+        for(int i=0;i<n;i++)
+        {
+            drawPlane(i*20);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void drawPlane(int posPlane)
+    {
+        pos.z=posPlane;
+        Instantiate(ground,pos,transform.rotation);
+    }
+}
