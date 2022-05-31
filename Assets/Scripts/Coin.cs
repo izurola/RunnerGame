b83aac99b1286341d286b4adc26d798f7571f640
Biligt <biligtgursed1027@gmail.com>
2022-05-31 19:12:18 +0800

    add coin collect fucntion

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -13,6 +13,14 @@ public class Coin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(40*Time.deltaTime,0,0);
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name=="Player")
+        {
+            Destroy(gameObject);
+        }
     }
 }
